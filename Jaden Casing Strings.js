eec7a49468f743e865fcebb8ef6a852616130b08
NouraNabile <nouranabile06@gmail.com>
2026-03-27 11:13:12 +0200

    kata7

--- Jaden Casing Strings.js
+++ Jaden Casing Strings.js
@@ -1,5 +1,10 @@
-String.prototype.tojadenCase=function(){
-    return this.split(" ").map((word)=>word[0].toUpperCase + word.substring(1)).join()
-}
-let str = "how old are you"
-console.log(str.tojadenCase())
+
+Object.defineProperty(
+    String.prototype,
+    'toJadenCase',
+    { value :
+    function toJadenCase() {
+        return this.split(" ").map((word)=>word[0].toUpperCase + word.substring(1)).join(" ")
+    }
+    }
+    );
